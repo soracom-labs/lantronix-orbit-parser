6342efcc406f95745f8abcbd883bc88b0d9d8273
“taketomatsunaga” <tm0683@gmail.com>
2022-06-30 18:04:55 +0900

    refactoring

--- assemblyscript/assembly/index.ts
+++ assemblyscript/assembly/index.ts
@@ -1,7 +1,7 @@
 import { setOutputJSON, log, getInputBufferAsString } from "orbit-sdk-assemblyscript";
 
 import { JSONEncoder } from "assemblyscript-json";
-import * as NmeaParser from "./nmea_parser";
+import * as Utils from "./utils";
 
 // $<sfal.pos _speed='0.00'>*43 $GPRMC,025904.000,V,3542.3459,N,13946.6537,E,,,250122,,*1D $<end> $<sfal.event _batt='3.312' _temp='28' _power='12.070' Ignition='1'>*4E $GPRMC,025904.000,V,3542.3459,N,13946.6537,E,,,250122,,*1D $<end>
 
@@ -16,7 +16,6 @@ export function uplink(): i32 {
   const lines = input.replaceAll("\r\n", "").split("$");
 
   const encoder = new JSONEncoder();
-  const isFlat = false;
   var isSfalPosAcquired = false;
   var isSfalEventAcquired = false;
   var isGPRMCAcquired = false;
@@ -28,82 +27,73 @@ export function uplink(): i32 {
     }
 
     const label = data[0];
-    var pre = "";
     if (label == "GPRMC" && !isGPRMCAcquired) {
       //GPRMC,045752.000,V,3542.3459,N,13946.6537,E,,,250122,,*16
       isGPRMCAcquired = true;
-      pre = "gprmc";
-      encoder.pushObject(pre);
-      pre = "";
+      encoder.pushObject("gprmc");
 
       for (let j = 0; j < data.length; ++j) {
-        encoder.setString(pre + "v" + j.toString(), data[j]);
+        encoder.setString("v" + j.toString(), data[j]);
       }
 
-      const latitude: number = NmeaParser.parseLatLon(data[3], data[4]);
+      const latitude: number = Utils.parseLatLon(data[3], data[4]);
       if (latitude !== 0) {
-        encoder.setFloat(pre + "latitude", latitude);
+        encoder.setFloat("latitude", latitude);
       }
 
-      const longitude: number = NmeaParser.parseLatLon(data[5], data[6]);
+      const longitude: number = Utils.parseLatLon(data[5], data[6]);
       if (longitude !== 0) {
-        encoder.setFloat(pre + "longitude", longitude);
+        encoder.setFloat("longitude", longitude);
       }
 
       encoder.popObject();
     } else if (label == "<sfal.pos" && !isSfalPosAcquired) {
       isSfalPosAcquired = true;
 
-      pre = "obdPos";
-      encoder.pushObject(pre);
-      pre = "";
+      encoder.pushObject("obdPos");
 
       for (let j = 0; j < data.length; ++j) {
         if (data[j].includes("_speed")) {
           //_speed='0.00'
-          const speed: string = NmeaParser.getValueFromKV(data[j]);
+          const speed: string = Utils.getValueFromKV(data[j]);
           if (speed != "") {
-            encoder.setFloat(pre + "speed", parseFloat(speed));
+            encoder.setFloat("speed", parseFloat(speed));
           }
         }
       }
 
-      if (!isFlat) {
-        encoder.popObject();
-      }
+      encoder.popObject();
 
       //<sfal.event _batt='3.312' _temp='28' _power='12.070' Ignition='1'>*4E
     } else if (label == "<sfal.event" && !isSfalEventAcquired) {
       isSfalEventAcquired = true;
 
-      pre = "obdEvent";
-      encoder.pushObject(pre);
-      pre = "";
+      encoder.pushObject("obdEvent");
 
       for (let j = 0; j < data.length; ++j) {
         if (data[j].includes("batt")) {
           //_batt='3.312'
-          const batt: string = NmeaParser.getValueFromKV(data[j]);
+          const batt: string = Utils.getValueFromKV(data[j]);
           if (batt != "") {
-            encoder.setFloat(pre + "batt", parseFloat(batt));
+            encoder.setFloat("batt", parseFloat(batt));
           }
         } else if (data[j].includes("temp")) {
           //_temp='28'
-          const temp: string = NmeaParser.getValueFromKV(data[j]);
+          const temp: string = Utils.getValueFromKV(data[j]);
           if (temp != "") {
-            encoder.setFloat(pre + "temp", parseFloat(temp));
+            encoder.setFloat("temp", parseFloat(temp));
           }
         } else if (data[j].includes("power")) {
           //_power='12.070'
-          const power: string = NmeaParser.getValueFromKV(data[j]);
+          const power: string = Utils.getValueFromKV(data[j]);
           if (power != "") {
-            encoder.setFloat(pre + "power", parseFloat(power));
+            encoder.setFloat("power", parseFloat(power));
           }
         } else if (data[j].includes("Ignition")) {
           //Ignition='1'>*4E
-          const ignition: string = NmeaParser.getValueFromKV(data[j]);
+          const ignition: string = Utils.getValueFromKV(data[j]);
           if (ignition != "") {
-            encoder.setFloat(pre + "ignition", parseFloat(ignition));
+            encoder.setFloat("ignition", parseFloat(ignition));
           }
         }
       }
